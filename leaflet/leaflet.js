d462633452ac256b0e4ba2fcf755c7cc5d65a65d
dougdaniels <dougdaniels@library.ucla.edu>
2019-01-08 12:40:43 -0800

    Update leaflet.js

--- leaflet/leaflet.js
+++ leaflet/leaflet.js
@@ -21,13 +21,14 @@ map = L.map('map', {
 
 
 // use this method to layer two separate image requests
- var layer1 = L.tileLayer.iiif('https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_natural_color_panorama_WIP2/info.json', {
+ var layer1 = L.tileLayer.iiif('http://iiif-kirschbombe.c9users.io/iiif/2/kabuki%2Fucla_bib1987273_no001_rs_001.jpg/info.json',
+                               {
      fitBounds: false,
   }
  ).addTo(map);
 
  var layer2 = L.tileLayer.iiif(
-   'https://stacks.stanford.edu/image/iiif/zf275jj8939%2FMartellus_300ppi_combined_processes_WIP2_panorama_with_blue_water/info.json'
+   'http://iiif-kirschbombe.c9users.io/iiif/2/kabuki%2Fucla_bib1987273_no001_rs_001.jpg/info.json'
 ).addTo(map);
 
 L.control.sideBySide(layer1, layer2).addTo(map);
